1e66c555e91c12508efdb1bcea8e8d2667b6e5a3
abrilmaldonado1 <129883601+abrilmaldonado1@users.noreply.github.com>
2023-05-09 14:02:49 -0300

    Add files via upload

---
+++ ej44.py
@@ -0,0 +1,15 @@
+nombre1= input ("ingrese el primer nombre: ")
+nombre2= input ("ingrese el primer nombre: ")
+if nombre1 < nombre2:
+    nombremayor=nombre2
+else:
+    nombremayor=nombre1
+if nombre1==nombre2:
+    nombremayor= "son iguales"
+print (nombremayor)
+
+
+
+
+
+  
